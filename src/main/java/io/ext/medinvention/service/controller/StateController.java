431c2f129253344c2fda5acb2ac379e6d25ab0b7
Marouan MOHAMED <medmarouen@gmail.com>
2019-12-30 15:46:37 +0100

    Fix style

--- src/main/java/io/ext/medinvention/service/controller/StateController.java
+++ src/main/java/io/ext/medinvention/service/controller/StateController.java
@@ -1,6 +1,5 @@
 package io.ext.medinvention.service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
